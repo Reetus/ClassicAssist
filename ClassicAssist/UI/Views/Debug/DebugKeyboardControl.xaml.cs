015dcb599c4dd93183d682c69b9f7af6efac0604
Reetus <reeeetus@gmail.com>
2021-01-26 07:16:43 +0700

    Keyboard key mapping debugger

---
+++ ClassicAssist/UI/Views/Debug/DebugKeyboardControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using ClassicAssist.UI.ViewModels.Debug;
+
+namespace ClassicAssist.UI.Views.Debug
+{
+    /// <summary>
+    ///     Interaction logic for DebugKeyboardControl.xaml
+    /// </summary>
+    public partial class DebugKeyboardControl : UserControl
+    {
+        public delegate void dKeyDown( KeyEventArgs e );
+
+        public DebugKeyboardControl()
+        {
+            DataContext = new DebugKeyboardViewModel( this );
+            InitializeComponent();
+        }
+
+        public event dKeyDown WPFKeyDownEvent;
+
+        protected override void OnKeyDown( KeyEventArgs e )
+        {
+            WPFKeyDownEvent?.Invoke( e );
+            base.OnKeyDown( e );
+        }
+    }
+}

9c04d004892c89155dc1ff5918d799a2b8fc6f43
Reetus <reeeetus@gmail.com>
2021-05-04 21:19:30 +0700

    No TaskBarIcon in mainwindow code behind

--- ClassicAssist/UI/Views/MainWindow.xaml.cs
+++ ClassicAssist/UI/Views/MainWindow.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Input;
 using Assistant;
-using ClassicAssist.Data;
-using Hardcodet.Wpf.TaskbarNotification;
 
 namespace ClassicAssist.UI.Views
 {
@@ -16,35 +14,6 @@ namespace ClassicAssist.UI.Views
             InitializeComponent();
             Engine.KeyboardLayoutId = InputLanguageManager.Current.CurrentInputLanguage.KeyboardLayoutId;
             MaxHeight = SystemParameters.VirtualScreenHeight - 35;
-
-            _taskbarIcon = new TaskbarIcon
-            {
-                Icon = Properties.Resources.cog,
-                ToolTipText = Shared.Resources.Strings.ProductName,
-                Visibility = Visibility.Visible,
-                DoubleClickCommand = TaskbarIconCommand,
-                DoubleClickCommandParameter = "DBL"
-            };
-
-            if (Options.CurrentOptions.SysTray)
-            {
-                _taskbarIcon.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                this.ShowInTaskbar = false;
-            }
-        }
-
-        private TaskbarIcon _taskbarIcon;
-        private ICommand _taskbarIconCommand;
-
-        public ICommand TaskbarIconCommand =>
-            _taskbarIconCommand ?? (_taskbarIconCommand = new Shared.UI.RelayCommand(TaskbarDoubleClick, o => true));
-
-        private void TaskbarDoubleClick(object obj)
-        {
-            this.Show();
         }
     }
 }

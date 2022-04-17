0ea47408b8c511689b86742fe33cc2640ff3c74b
Reetus <6239195+Reetus@users.noreply.github.com>
2022-04-17 16:08:51 +0700

    Updater to install prereleases if option selected

--- ClassicAssist.Shared/Settings.Designer.cs
+++ ClassicAssist.Shared/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace ClassicAssist.Shared {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.0.3.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.1.0.0")]
     public sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -68,6 +68,18 @@ namespace ClassicAssist.Shared {
             }
             set {
                 this["UpdateManifestURL"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("https://classicassist.azurewebsites.net/prerelease.json")]
+        public string PrereleaseManifestURL {
+            get {
+                return ((string)(this["PrereleaseManifestURL"]));
+            }
+            set {
+                this["PrereleaseManifestURL"] = value;
             }
         }
     }

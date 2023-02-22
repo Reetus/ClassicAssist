0ee49cdd21ec750488614df110d4c45dd4611e2e
Reetus <6239195+Reetus@users.noreply.github.com>
2023-02-22 09:18:28 +0700

    WIP ItemID Filter (#338)
    * WIP ItemID Filter
    
    * Support old worlditem packet
    
    * UI Changes

---
+++ ClassicAssist/UI/ViewModels/Filters/ItemIDFilterConfigureWindowViewModel.cs
@@ -0,0 +1,173 @@
+﻿#region License
+
+// Copyright (C) 2023 Reetus
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY
+
+#endregion
+
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Assistant;
+using ClassicAssist.Data.Filters;
+using ClassicAssist.Shared.UI;
+using ClassicAssist.UI.Views;
+using ClassicAssist.UI.Views.Filters.ItemIDFilter;
+using ClassicAssist.UO;
+using ClassicAssist.UO.Data;
+
+namespace ClassicAssist.UI.ViewModels.Filters
+{
+    public class ItemIDFilterConfigureWindowViewModel : BaseViewModel
+    {
+        private ICommand _addCommand;
+        private ObservableCollection<ItemIDFilterEntry> _items = new ObservableCollection<ItemIDFilterEntry>();
+        private ICommand _removeCommand;
+        private ICommand _selectDestinationIDCommand;
+        private ItemIDFilterEntry _selectedItem;
+        private ICommand _selectSourceIDCommand;
+        private ICommand _targetDestinationIDCommand;
+        private ICommand _targetSourceIDCommand;
+        private ICommand _selectHueCommand;
+
+        public ItemIDFilterConfigureWindowViewModel( ObservableCollection<ItemIDFilterEntry> items )
+        {
+            Items = items;
+        }
+
+        public ItemIDFilterConfigureWindowViewModel()
+        {
+            Items.Add( new ItemIDFilterEntry() );
+        }
+
+        public ICommand AddCommand => _addCommand ?? ( _addCommand = new RelayCommand( Add, o => true ) );
+
+        public ObservableCollection<ItemIDFilterEntry> Items
+        {
+            get => _items;
+            set => SetProperty( ref _items, value );
+        }
+
+        public ICommand RemoveCommand =>
+            _removeCommand ?? ( _removeCommand = new RelayCommand( Remove, o => o != null ) );
+
+        public ICommand SelectDestinationIDCommand =>
+            _selectDestinationIDCommand ??
+            ( _selectDestinationIDCommand = new RelayCommand( SelectDestinationID, o => o != null ) );
+
+        public ItemIDFilterEntry SelectedItem
+        {
+            get => _selectedItem;
+            set => SetProperty( ref _selectedItem, value );
+        }
+
+        public ICommand SelectSourceIDCommand =>
+            _selectSourceIDCommand ?? ( _selectSourceIDCommand = new RelayCommand( SelectSourceID, o => o != null ) );
+
+        public ICommand TargetDestinationIDCommand =>
+            _targetDestinationIDCommand ?? ( _targetDestinationIDCommand =
+                new RelayCommandAsync( TargetDestinationID, o => o != null && Engine.Connected ) );
+
+        public ICommand TargetSourceIDCommand =>
+            _targetSourceIDCommand ?? ( _targetSourceIDCommand =
+                new RelayCommandAsync( TargetSourceID, o => o != null && Engine.Connected ) );
+
+        private static async Task TargetDestinationID( object arg )
+        {
+            if ( !( arg is ItemIDFilterEntry entry ) )
+            {
+                return;
+            }
+
+            ( TargetType _, TargetFlags _, int _, int _, int _, int _, int itemID ) =
+                await Commands.GetTargetInfoAsync();
+
+            entry.DestinationID = itemID;
+        }
+
+        private static void SelectDestinationID( object obj )
+        {
+            if ( !( obj is ItemIDFilterEntry entry ) )
+            {
+                return;
+            }
+
+            ItemIDSelectionWindow window = new ItemIDSelectionWindow();
+
+            window.ShowDialog();
+
+            if ( window.Result )
+            {
+                entry.DestinationID = window.SelectedItem.ID;
+            }
+        }
+
+        private static async Task TargetSourceID( object arg )
+        {
+            if ( !( arg is ItemIDFilterEntry entry ) )
+            {
+                return;
+            }
+
+            ( TargetType _, TargetFlags _, int _, int _, int _, int _, int itemID ) =
+                await Commands.GetTargetInfoAsync();
+
+            entry.SourceID = itemID;
+        }
+
+        public ICommand SelectHueCommand =>
+            _selectHueCommand ?? ( _selectHueCommand = new RelayCommand( SelectHue, o => o != null ) );
+
+        private static void SelectHue( object obj )
+        {
+            if ( !( obj is ItemIDFilterEntry entry ) )
+            {
+                return;
+            }
+
+            if ( HuePickerWindow.GetHue( out int hue ) )
+            {
+                entry.Hue = hue;
+            }
+        }
+
+        private static void SelectSourceID( object obj )
+        {
+            if ( !( obj is ItemIDFilterEntry entry ) )
+            {
+                return;
+            }
+
+            ItemIDSelectionWindow window = new ItemIDSelectionWindow();
+
+            window.ShowDialog();
+
+            if ( window.Result )
+            {
+                entry.SourceID = window.SelectedItem.ID;
+            }
+        }
+
+        private void Add( object obj )
+        {
+            Items.Add( new ItemIDFilterEntry() );
+        }
+
+        private void Remove( object obj )
+        {
+            if ( !( obj is ItemIDFilterEntry entry ) )
+            {
+                return;
+            }
+
+            Items.Remove( entry );
+        }
+    }
+}
---
+++ ClassicAssist/UI/Views/Filters/ItemIDFilter/ItemIDSelectionWindow.xaml.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media;
+using ClassicAssist.Misc;
+using ClassicAssist.Shared.UI;
+using ClassicAssist.UO.Data;
+using Microsoft.Scripting.Utils;
+
+namespace ClassicAssist.UI.Views.Filters.ItemIDFilter
+{
+    /// <summary>
+    ///     Interaction logic for ItemIDSelectionWindow.xaml
+    /// </summary>
+    public partial class ItemIDSelectionWindow : INotifyPropertyChanged
+    {
+        private bool _imagesLoaded;
+        private ICommand _okCommand;
+        private string _searchText;
+        private ImageData _selectedItem;
+
+        public ItemIDSelectionWindow()
+        {
+            InitializeComponent();
+
+            LoadImageData().ConfigureAwait( false );
+        }
+
+        public ObservableCollection<ImageData> FilterImages { get; set; } = new ObservableCollection<ImageData>();
+        public ObservableCollection<ImageData> Images { get; set; } = new ObservableCollection<ImageData>();
+
+        public bool ImagesLoaded
+        {
+            get => _imagesLoaded;
+            set => SetField( ref _imagesLoaded, value );
+        }
+
+        public ICommand OKCommand => _okCommand ?? ( _okCommand = new RelayCommand( OK, o => o != null ) );
+
+        public bool Result { get; set; }
+
+        public string SearchText
+        {
+            get => _searchText;
+            set
+            {
+                SetField( ref _searchText, value );
+                UpdateFilter();
+            }
+        }
+
+        public ImageData SelectedItem
+        {
+            get => _selectedItem;
+            set => SetField( ref _selectedItem, value );
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private Task LoadImageData()
+        {
+            return Task.Run( () =>
+            {
+                List<ImageData> images = new List<ImageData>();
+
+                for ( int i = 0; i < 10000; i++ )
+                {
+                    ImageSource image = Art.GetStatic( i ).ToImageSource();
+                    image.Freeze();
+
+                    images.Add( new ImageData
+                    {
+                        ID = i, ImageSource = image, Name = TileData.GetStaticTile( i ).Name
+                    } );
+                }
+
+                ImagesLoaded = true;
+
+                Dispatcher.Invoke( () =>
+                {
+                    Images.AddRange( images );
+                    UpdateFilter();
+                } );
+            } );
+        }
+
+        private void UpdateFilter()
+        {
+            if ( string.IsNullOrEmpty( SearchText ) )
+            {
+                FilterImages.AddRange( Images );
+                return;
+            }
+
+            List<ImageData> filterItems = Images.Where( e => e.Name.Contains( SearchText ) ).ToList();
+
+            if ( int.TryParse( SearchText, out int searchInt ) )
+            {
+                filterItems.AddRange( Images.Where( e => e.ID.Equals( searchInt ) ) );
+            }
+
+            if ( SearchText.StartsWith( "0x" ) && SearchText.Length >= 3 )
+            {
+                int int32 = Convert.ToInt32( SearchText, 16 );
+
+                filterItems.AddRange( Images.Where( e => e.ID.Equals( int32 ) ) );
+            }
+
+            FilterImages.Clear();
+            FilterImages.AddRange( filterItems );
+        }
+
+        protected virtual void OnPropertyChanged( [CallerMemberName] string propertyName = null )
+        {
+            PropertyChanged?.Invoke( this, new PropertyChangedEventArgs( propertyName ) );
+        }
+
+        protected bool SetField<T>( ref T field, T value, [CallerMemberName] string propertyName = null )
+        {
+            if ( EqualityComparer<T>.Default.Equals( field, value ) )
+            {
+                return false;
+            }
+
+            field = value;
+            OnPropertyChanged( propertyName );
+            return true;
+        }
+
+        private void OK( object obj )
+        {
+            Result = true;
+        }
+
+        public class ImageData
+        {
+            public int ID { get; set; }
+            public ImageSource ImageSource { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}

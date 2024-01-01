21bb34648799dc3ea75b0a17518eaf5088805997
Reetus <6239195+Reetus@users.noreply.github.com>
2024-01-01 13:15:48 +0700

    Don't organize destination container descendants (when destination is in source) (Fixes #385) (#387)

--- ClassicAssist.Tests/Agents/OrganizerTests.cs
+++ ClassicAssist.Tests/Agents/OrganizerTests.cs
@@ -37,9 +37,14 @@ namespace ClassicAssist.Tests.Agents
         {
             Engine.Items.Clear();
 
-            Item container = new Item( 0x40000000 ) { Container = new ItemCollection( 0x40000000 ) };
-            container.Container.Add( new Item( 0x40000001 ) { ID = 0xf06, Hue = 1000 } );
-            container.Container.Add( new Item( 0x40000002 ) { ID = 0xf06, Hue = 1161 } );
+            Item container = new Item( 0x40000000 )
+            {
+                Container = new ItemCollection( 0x40000000 )
+                {
+                    new Item( 0x40000001 ) { ID = 0xf06, Hue = 1000 },
+                    new Item( 0x40000002 ) { ID = 0xf06, Hue = 1161 }
+                }
+            };
             Engine.Items.Add( container );
 
             Engine.PacketWaitEntries = new PacketWaitEntries();
@@ -50,6 +55,20 @@ namespace ClassicAssist.Tests.Agents
             entry.Items.Add( new OrganizerItem { ID = 0xf06, Hue = -1, Amount = -1, Item = "Test" } );
 
             AutoResetEvent are = new AutoResetEvent( false );
+
+            Engine.InternalPacketSentEvent += OnInternalPacketSentEvent;
+
+            Task.Run( () => manager.Organize( entry, container.Serial, container.Serial ) ).Wait();
+
+            bool result = are.WaitOne( 4000 );
+
+            Assert.IsTrue( result );
+
+            manager.Items.Clear();
+            Engine.InternalPacketSentEvent -= OnInternalPacketSentEvent;
+            Engine.PacketWaitEntries = null;
+            Engine.Items.Clear();
+            return;
 
             void OnInternalPacketSentEvent( byte[] data, int length )
             {
@@ -70,6 +89,31 @@ namespace ClassicAssist.Tests.Agents
                     are.Set();
                 }
             }
+        }
+
+        [TestMethod]
+        public void WillOrganizeOnlyHue()
+        {
+            Engine.Items.Clear();
+
+            Item container = new Item( 0x40000000 )
+            {
+                Container = new ItemCollection( 0x40000000 )
+                {
+                    new Item( 0x40000001 ) { ID = 0xf06, Hue = 1000 },
+                    new Item( 0x40000002 ) { ID = 0xf06, Hue = 1161 }
+                }
+            };
+            Engine.Items.Add( container );
+
+            Engine.PacketWaitEntries = new PacketWaitEntries();
+
+            OrganizerManager manager = OrganizerManager.GetInstance();
+
+            OrganizerEntry entry = new OrganizerEntry();
+            entry.Items.Add( new OrganizerItem { ID = 0xf06, Hue = 1161, Amount = -1, Item = "Test" } );
+
+            AutoResetEvent are = new AutoResetEvent( false );
 
             Engine.InternalPacketSentEvent += OnInternalPacketSentEvent;
 
@@ -83,26 +127,7 @@ namespace ClassicAssist.Tests.Agents
             Engine.InternalPacketSentEvent -= OnInternalPacketSentEvent;
             Engine.PacketWaitEntries = null;
             Engine.Items.Clear();
-        }
-
-        [TestMethod]
-        public void WillOrganizeOnlyHue()
-        {
-            Engine.Items.Clear();
-
-            Item container = new Item( 0x40000000 ) { Container = new ItemCollection( 0x40000000 ) };
-            container.Container.Add( new Item( 0x40000001 ) { ID = 0xf06, Hue = 1000 } );
-            container.Container.Add( new Item( 0x40000002 ) { ID = 0xf06, Hue = 1161 } );
-            Engine.Items.Add( container );
-
-            Engine.PacketWaitEntries = new PacketWaitEntries();
-
-            OrganizerManager manager = OrganizerManager.GetInstance();
-
-            OrganizerEntry entry = new OrganizerEntry();
-            entry.Items.Add( new OrganizerItem { ID = 0xf06, Hue = 1161, Amount = -1, Item = "Test" } );
-
-            AutoResetEvent are = new AutoResetEvent( false );
+            return;
 
             void OnInternalPacketSentEvent( byte[] data, int length )
             {
@@ -132,10 +157,45 @@ namespace ClassicAssist.Tests.Agents
                     }
                 }
             }
+        }
+
+        [TestMethod]
+        public void WontOrganizeDestinationContainerDescendants()
+        {
+            // Destination container is descendant of source container
+            // Don't organize destination container items
+            Engine.Items.Clear();
+            Engine.PacketWaitEntries = new PacketWaitEntries();
+
+            Item sourceContainer = new Item( 0x40000000 )
+            {
+                Container = new ItemCollection( 0x40000000 )
+                {
+                    new Item( 0x40000003 ) { ID = 0x1f14, Owner = 0x40000000 }
+                }
+            };
+
+            Item destinationContainer = new Item( 0x40000001 )
+            {
+                Container = new ItemCollection( 0x40000001 )
+                {
+                    new Item( 0x40000004 ) { ID = 0x1f14, Owner = 0x40000001 }
+                }
+            };
+
+            sourceContainer.Container.Add( destinationContainer );
+            Engine.Items.Add( sourceContainer );
+
+            OrganizerManager manager = OrganizerManager.GetInstance();
+
+            OrganizerEntry entry = new OrganizerEntry();
+            entry.Items.Add( new OrganizerItem { ID = 0x1f14, Hue = -1, Amount = -1, Item = "Test" } );
+
+            AutoResetEvent are = new AutoResetEvent( false );
 
             Engine.InternalPacketSentEvent += OnInternalPacketSentEvent;
 
-            Task.Run( () => manager.Organize( entry, container.Serial, container.Serial ) ).Wait();
+            Task.Run( () => manager.Organize( entry, sourceContainer.Serial, destinationContainer.Serial ) ).Wait();
 
             bool result = are.WaitOne( 4000 );
 
@@ -145,6 +205,91 @@ namespace ClassicAssist.Tests.Agents
             Engine.InternalPacketSentEvent -= OnInternalPacketSentEvent;
             Engine.PacketWaitEntries = null;
             Engine.Items.Clear();
+
+            return;
+
+            void OnInternalPacketSentEvent( byte[] data, int length )
+            {
+                if ( data[0] == 0x07 )
+                {
+                    int serial = ( data[1] << 24 ) | ( data[2] << 16 ) | ( data[3] << 8 ) | data[4];
+
+                    if ( serial == 0x40000003 )
+                    {
+                        are.Set();
+                    }
+                    else
+                    {
+                        Assert.Fail();
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void WillOrganizeSourceInDestination()
+        {
+            // Source container is descendant of destination container
+            // Organize source container items
+            Engine.Items.Clear();
+            Engine.PacketWaitEntries = new PacketWaitEntries();
+
+            Item sourceContainer = new Item( 0x40000000 )
+            {
+                Container = new ItemCollection( 0x40000000 )
+                {
+                    new Item( 0x40000003 ) { ID = 0x1f14, Owner = 0x40000000 }
+                }
+            };
+
+            Item destinationContainer = new Item( 0x40000001 )
+            {
+                Container = new ItemCollection( 0x40000001 )
+                {
+                    new Item( 0x40000004 ) { ID = 0x1f14, Owner = 0x40000001 }, sourceContainer
+                }
+            };
+
+            Engine.Items.Add( destinationContainer );
+
+            OrganizerManager manager = OrganizerManager.GetInstance();
+
+            OrganizerEntry entry = new OrganizerEntry();
+            entry.Items.Add( new OrganizerItem { ID = 0x1f14, Hue = -1, Amount = -1, Item = "Test" } );
+
+            AutoResetEvent are = new AutoResetEvent( false );
+
+            Engine.InternalPacketSentEvent += OnInternalPacketSentEvent;
+
+            Task.Run( () => manager.Organize( entry, sourceContainer.Serial, destinationContainer.Serial ) ).Wait();
+
+            bool result = are.WaitOne( 4000 );
+
+            Assert.IsTrue( result );
+
+            manager.Items.Clear();
+            Engine.InternalPacketSentEvent -= OnInternalPacketSentEvent;
+            Engine.PacketWaitEntries = null;
+            Engine.Items.Clear();
+
+            return;
+
+            void OnInternalPacketSentEvent( byte[] data, int length )
+            {
+                if ( data[0] == 0x07 )
+                {
+                    int serial = ( data[1] << 24 ) | ( data[2] << 16 ) | ( data[3] << 8 ) | data[4];
+
+                    if ( serial == 0x40000003 )
+                    {
+                        are.Set();
+                    }
+                    else
+                    {
+                        Assert.Fail();
+                    }
+                }
+            }
         }
     }
 }

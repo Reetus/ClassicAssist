29a978c2dd3eb7b81ddeba947af0c1ee61726c0b
Reetus <reeeetus@gmail.com>
2024-01-03 19:30:43 +0700

    ECV target multi items / move to bank

--- ClassicAssist/UO/Network/PacketFilter/PacketWaitEntry.cs
+++ ClassicAssist/UO/Network/PacketFilter/PacketWaitEntry.cs
@@ -6,7 +6,7 @@ namespace ClassicAssist.UO.Network.PacketFilter
     {
         public bool AutoRemove { get; set; }
         public AutoResetEvent Lock { get; set; }
-        public bool MatchFilteredOnly { get; set; } = false;
+        public bool MatchInternal { get; set; } = false;
         public byte[] Packet { get; set; }
         public PacketDirection PacketDirection { get; set; }
         public PacketFilterInfo PFI { get; set; }

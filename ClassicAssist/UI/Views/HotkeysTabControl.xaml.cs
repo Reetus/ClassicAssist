de0b3b09539fd6cc5078a6d99995b28c37fd6814
Reetus <reeeetus@gmail.com>
2024-01-10 22:25:46 +0700

    Send mousewheel to parent ScrollViewer

--- ClassicAssist/UI/Views/HotkeysTabControl.xaml.cs
+++ ClassicAssist/UI/Views/HotkeysTabControl.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace ClassicAssist.UI.Views
 {
@@ -10,6 +13,41 @@ namespace ClassicAssist.UI.Views
         public HotkeysTabControl()
         {
             InitializeComponent();
+        }
+
+        private void UIElement_OnPreviewMouseWheel( object sender, MouseWheelEventArgs e )
+        {
+            if ( e.Handled )
+            {
+                return;
+            }
+
+            e.Handled = true;
+            MouseWheelEventArgs eventArg = new MouseWheelEventArgs( e.MouseDevice, e.Timestamp, e.Delta ) { RoutedEvent = MouseWheelEvent, Source = sender };
+            ScrollViewer parent = GetScrollViewer( (Control) sender );
+            parent.RaiseEvent( eventArg );
+        }
+
+        public static ScrollViewer GetScrollViewer( DependencyObject depObj )
+        {
+            if ( depObj is ScrollViewer scrollViewer )
+            {
+                return scrollViewer;
+            }
+
+            for ( int i = 0; i < VisualTreeHelper.GetChildrenCount( depObj ); i++ )
+            {
+                DependencyObject child = VisualTreeHelper.GetChild( depObj, i );
+
+                ScrollViewer result = GetScrollViewer( child );
+
+                if ( result != null )
+                {
+                    return result;
+                }
+            }
+
+            return null;
         }
     }
 }

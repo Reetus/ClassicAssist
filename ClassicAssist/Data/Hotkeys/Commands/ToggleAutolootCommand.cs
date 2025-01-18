98d6438ed83a40ed44de3edfb2db10999dad8791
Reetus <reeeetus@gmail.com>
2025-01-18 10:11:41 +0700

    Add missing command

---
+++ ClassicAssist/Data/Hotkeys/Commands/ToggleAutolootCommand.cs
@@ -0,0 +1,27 @@
+﻿#region License
+
+// Copyright (C) 2025 Reetus
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY
+
+#endregion
+
+using ClassicAssist.Data.Macros.Commands;
+
+namespace ClassicAssist.Data.Hotkeys.Commands
+{
+    [HotkeyCommand( Name = "Toggle Autoloot", Category = "Agents" )]
+    public class ToggleAutolootCommand : HotkeyCommand
+    {
+        public override void Execute()
+        {
+            AgentCommands.SetAutoloot();
+        }
+    }
+}

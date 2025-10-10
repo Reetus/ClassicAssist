6cdb301b451de6218ee873ba0a756504177e2b23
Reetus <reeeetus@gmail.com>
2025-10-10 11:26:33 +0700

    Fix license header

--- ClassicAssist/UI/Views/ECV/Settings/Models/ContainerSet.cs
+++ ClassicAssist/UI/Views/ECV/Settings/Models/ContainerSet.cs
@@ -1,6 +1,6 @@
 ﻿#region License
 
-// Copyright (C) $CURRENT_YEAR$ Reetus
+// Copyright (C) 2025 Reetus
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -8,11 +8,15 @@
 // (at your option) any later version.
 // 
 // This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #endregion
 
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using ClassicAssist.Shared.UI;
 
